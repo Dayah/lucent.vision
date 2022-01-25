983990a6a5851c749425a8305343680881d7ddf4
Michael Dayah <michael@dayah.com>
2022-01-25 23:48:43 +0000

    Pushing to find gulpfile error.

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,42 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var cache = require('gulp-cache');
-var cp = require('child_process');
+var spawn = require('child_process').spawn;
 var browserSync = require('browser-sync');
 
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var jekyll = 'jekyll';
 
 // Build the Jekyll Site
 gulp.task('jekyll-build', function (done) {
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+    return spawn(jekyll, ['build'], {stdio: 'inherit'})
         .on('close', done);
 });
 
 // Rebuild Jekyll and page reload
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+gulp.task('jekyll-rebuild', function () {
     browserSync.reload();
 });
 
+// Compile files
+gulp.task('sass', function () {
+    return gulp.src('assets/css/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('_site/assets/css'));
+        //.pipe(browserSync.reload({stream:true}));
+        //.pipe(gulp.dest('assets/css'));
+});
+
 // Wait for jekyll-build, then launch the Server
-gulp.task('browser-sync', ['sass', 'img', 'jekyll-build'], function() {
+gulp.task('browser-sync', gulp.series('sass', 'img', 'jekyll-build'), function() {
     browserSync({
         server: {
             baseDir: '_site'
         },
         notify: false
     });
-});
-
-// Compile files
-gulp.task('sass', function () {
-    return gulp.src('assets/css/scss/main.scss')
-        .pipe(sass({
-            outputStyle: 'expanded',
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/assets/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('assets/css'));
+    console.log("browser sync running");
 });
 
 // Compression images
@@ -58,11 +54,11 @@ gulp.task('img', function() {
 
 // Watch scss, html, img files
 gulp.task('watch', function () {
-    gulp.watch('assets/css/scss/**/*.scss', ['sass']);
-    gulp.watch('assets/js/**/*.js', ['jekyll-rebuild']);
-    gulp.watch('assets/img/**/*', ['img']);
-    gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_pages/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch('assets/css/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('assets/js/**/*.js', gulp.series('jekyll-rebuild'));
+    gulp.watch('assets/img/**/*', gulp.series('img'));
+    gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_pages/*.html', '_posts/*'], gulp.series('jekyll-rebuild'));
 });
 
 //  Default task
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', gulp.series('browser-sync', 'watch'));
